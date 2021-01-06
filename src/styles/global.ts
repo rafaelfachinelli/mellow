1a684d83dcab80fafd4426fd2eebf012e0315433
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-01-06 17:23:30 -0300

    colors theme added

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,16 @@ export default createGlobalStyle`
 
 :root {
   --color-primary: #2BCFA2;
+  --color-red: #DF3558;
+
+  --color-text-title: #00A35E;
+  --color-text-lighter: #FFFFFF;
+  --color-text-darker: #2C3834;
+
+  --color-background-button: #00EA87;
+  --color-background-footer: #3B3B3B;
+
+  --color-placeholder-input: #7B7B7B;
 }
 
 * {
