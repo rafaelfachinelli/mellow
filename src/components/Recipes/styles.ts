6bcf62e7197f06f4511af506c13f49439645e3a6
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-01-09 02:28:08 -0300

    fix: padding recipes + overflow carousel

--- src/components/Recipes/styles.ts
+++ src/components/Recipes/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   min-height: 108rem;
-  padding: 0 3.5rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -17,8 +16,7 @@ export const Container = styled.div`
 
   .recipes__description {
     max-width: 1320px;
-    margin: 2rem 0 5rem 0;
-
+    margin: 2rem 3.5rem 5rem 3.5rem;
     color: var(--color-text-darker);
     text-align: center;
     font-size: 2rem;
