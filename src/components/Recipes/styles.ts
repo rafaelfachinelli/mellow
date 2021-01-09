fc09b9828a27287e9b890c78e255ac0add4f20a0
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-01-09 19:34:19 -0300

    fix: height slick track

--- src/components/Recipes/styles.ts
+++ src/components/Recipes/styles.ts
@@ -11,12 +11,12 @@ export const Container = styled.div`
   .recipes__title {
     color: var(--color-text-darker);
     font-size: 5rem;
-    font-weight: 600;
+    font-weight: 500;
   }
 
   .recipes__description {
     max-width: 1320px;
-    margin: 2rem 3.5rem 5rem 3.5rem;
+    margin: 4rem 3.5rem 7rem 3.5rem;
     color: var(--color-text-darker);
     text-align: center;
     font-size: 2rem;
